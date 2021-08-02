7404168379d01895443cb2a765cd1d593be13783
vipin <vipinpublicdomain@gmail.com>
2021-08-02 22:28:59 +0530

    Update JenkinsSamaple.java

--- JenkinsSamaple.java
+++ JenkinsSamaple.java
@@ -2,7 +2,7 @@
 public class JenkinsSamaple {
 
 	public static void main(String[] args) {
-		System.out.println("** USing WEbHOok try ******");
+		System.out.println("** USing WEbHOok ffffftry ******");
 
 	}
 
