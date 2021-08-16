4498148de058f7e611c4c829ad023d4cf74e3b72
vipin <vipinpublicdomain@gmail.com>
2021-08-16 20:47:10 +0530

    Update JenkinsSamaple.java

--- JenkinsSamaple.java
+++ JenkinsSamaple.java
@@ -2,7 +2,7 @@
 public class JenkinsSamaple {
 
 	public static void main(String[] args) {
-		System.out.println("** USing Saple ateafadf  ffffftry ******");
+		System.out.println("** USing Saple ateafadf  Chek hok ******");
 
 	}
 
